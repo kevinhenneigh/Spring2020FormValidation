0bdfe36312a86896743b6c8b670b15595128ac39
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-04-16 14:43:52 -0700

    Created  typescript file, started form validation

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    var formBtn = document.querySelector("button");
+    formBtn.onclick = main;
+};
+function main() {
+    alert("Reg button was clicked");
+}
---
+++ ts/main.ts
@@ -0,0 +1,9 @@
+
+window.onload = function(){
+    let formBtn = <HTMLElement>document.querySelector("button");
+    formBtn.onclick = main
+}
+
+function main():void{
+    alert("Reg button was clicked");
+}
